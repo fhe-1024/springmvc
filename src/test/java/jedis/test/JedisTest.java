7692df0bda1fa78863d5aacb1e9a8582e2c35e08
1042302758@qq.com <1042302758@qq.com>
2016-07-20 17:44:02 +0800

    jedis test

--- src/test/java/jedis/test/JedisTest.java
+++ src/test/java/jedis/test/JedisTest.java
@@ -6,8 +6,13 @@ import redis.clients.jedis.JedisPoolConfig;
 
 public class JedisTest {
 	public static void main(String[] args) {
+		for (int i = 0; i < 5000; i++) {
+			JedisUtil.set("" + i, "" + i);
 
-		JedisUtil.set("hello", "world");
-		System.out.println(JedisUtil.get("hello"));
+		}
+		for (int i = 0; i < 5000; i++) {
+
+			System.out.println(JedisUtil.get("" + i));
+		}
 	}
 }
