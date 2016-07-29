a1cd680198ceaf67d20b36d8e1fd88c72fe06add
1042302758@qq.com <1042302758@qq.com>
2016-07-29 14:18:43 +0800

    update commons-httpclient post.

--- src/test/java/http/commons_httpclient/HttpUtil.java
+++ src/test/java/http/commons_httpclient/HttpUtil.java
@@ -7,16 +7,20 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.http.entity.StringEntity;
 
 public class HttpUtil {
 
 	public static String get(String url) throws Exception {
-		
+
 		HttpClient client = new HttpClient();
 		client.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "UTF-8");
 		client.getHttpConnectionManager().getParams().setConnectionTimeout(1000 * 500);
@@ -39,13 +43,15 @@ public class HttpUtil {
 		return sb.toString();
 	}
 
-	public static String post(String url, Map<String, Object> map) throws Exception {
+	public static String post(String url, String transjson) throws Exception {
 		HttpClient client = new HttpClient();
 		client.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "utf-8");
 		client.getHttpConnectionManager().getParams().setConnectionTimeout(1000 * 500);
 		PostMethod postMethod = new PostMethod(url);
-		// postMethod.addParameter("hello", "world");
-
+		RequestEntity re = new StringRequestEntity(transjson, "application/json", "utf-8");
+		System.out.println(re.getContentType());
+		postMethod.setRequestEntity(re);
+		postMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());// 调用失败会重试
 		int statuscode = client.executeMethod(postMethod);
 		if (statuscode != HttpStatus.SC_OK) {
 			throw new RuntimeException(postMethod.getStatusLine().toString());
@@ -67,8 +73,7 @@ public class HttpUtil {
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		try {
 			// System.out.println(HttpUtil.get("https://baidu.com"));
-			System.out.println(HttpUtil.post("http://zj.callkr.com:8050/crm/ourJs/Ynotify/js/Ynotify.js",
-					new HashMap<String, Object>()));
+			System.out.println(HttpUtil.post("http://zj.callkr.com:8050/crm/ourJs/Ynotify/js/Ynotify.js", ""));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
